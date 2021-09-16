206c7aa431ecdf03850a92cc1b6fc7803398cd1f
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-09-16 17:54:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,22 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(1280, 300);
     video.hide();
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
 
+function modelLoaded(){
+    console.log("poseNet is loaded");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        console.log("nose x: " + noseX);
+        console.log("nose y: " + noseY);
+    }
 }
 
 function draw(){
